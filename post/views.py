b208c0fd5bf456d5b2859f8ecf1122233c2433d6
Sebas16608 <asebasrr444@gmail.com>
2025-11-06 23:55:58 -0600

    Definiendo el metodo PUT de la API de acticle

--- post/views.py
+++ post/views.py
@@ -71,3 +71,16 @@ class ArticleView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        try:
+            article = Article.objects.get(pk=pk)
+        except Article.DoesNotExist:
+            return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
