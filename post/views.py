24f621d047e601bc0c374ef294de8a31363eca71
Sebas16608 <asebasrr444@gmail.com>
2025-11-06 23:47:20 -0600

    Definiendo el metodo POST de la API de articulos

--- post/views.py
+++ post/views.py
@@ -64,4 +64,10 @@ class ArticleView(APIView):
             article = Article.objects.all()
             serializer = ArticleSerializer(article, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
-        
+    
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
