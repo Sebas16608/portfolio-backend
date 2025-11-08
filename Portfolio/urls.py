a9d3e135a4134f711b455985493a93d72a8fe8b0
Sebas16608 <asebasrr444@gmail.com>
2025-11-08 16:16:40 -0600

    creando la ruta de mensajes

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('post/', include("post.urls"))
+    path('post/', include("post.urls")),
+    path('mensaje/', include("mensaje.urls"))
 ]
