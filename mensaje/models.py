66685c95706ceab99c2a6bf81130eba3b95af291
Sebas16608 <asebasrr444@gmail.com>
2025-11-07 18:49:03 -0600

    Creando los modelos de la app mensaje

--- mensaje/models.py
+++ mensaje/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Mensaje(models.Model):
+    nombre = models.CharField(max_length=255)
+    email = models.EmailField()
+    mensaje = models.TextField()
+    creado = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Mensaje"
+        verbose_name_plural = "Mensajes"
+
+    def __str__(self):
+        return f"Mensaje de {self.nombre}"
