b9aa168564ee0af870b54aca65a8fe9a4eca49ca
Sebas16608 <asebasrr444@gmail.com>
2025-12-18 22:33:43 -0600

    Iniciando todo desde cero

--- post/urls.py
+++
@@ -1,9 +0,0 @@
-from .views import CategoryView, ArticleView
-from django.urls import path
-
-urlpatterns = [
-    path("category/", CategoryView.as_view(), name="categoria-list"),
-    path("category/<int:pk>/", CategoryView.as_view(), name="category-detail"),
-    path("article/", ArticleView.as_view(), name="articulo-list"),
-    path("article/<int:pk>/", ArticleView.as_view(), name="articulo-detail")
-]
