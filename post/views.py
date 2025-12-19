2bf09175a28c065556a18d48e741879e38b5f5bf
Sebas16608 <asebasrr444@gmail.com>
2025-12-18 23:29:39 -0600

    Definiendo las APIs de cada modelo

--- post/views.py
+++ post/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from API import SuperAPIView
+from .serializers import CategorySerializer, ArticleSerializer
+from .models import Category, Article
 # Create your views here.
+class CategoryView(SuperAPIView):
+    model = Category
+    serializer = CategorySerializer
+
+class AricleView(SuperAPIView):
+    model = Article
+    serializer = ArticleSerializer
