50395a2b29fc18e38eb035e543e3d2f1488a67b0
Sebas16608 <asebasrr444@gmail.com>
2025-12-23 21:59:43 -0600

    Usaremos un urlfield para las imagenes

--- post/models.py
+++ post/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
-    image = models.ImageField(upload_to="")
+    image = models.URLField()
     featured = models.BooleanField(default=False)
 
     class Meta:
@@ -19,7 +19,7 @@ class Article(models.Model):
     name = models.CharField(max_length=255)
     introduccion = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
-    image = models.ImageField(upload_to="")
+    image = models.URLField()
     content = models.TextField()
     ESTADOS = [
         ("PRODUCCION", "Producción"),
